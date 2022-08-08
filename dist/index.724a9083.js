a5d880896fa20ad64c40e95edfb63fb86fc5bec7
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2022-08-08 15:24:11 -0700

    Car

--- dist/index.724a9083.js
+++ dist/index.724a9083.js
@@ -6,18 +6,38 @@ class Car {
         this.height = height;
         this.speed = 0;
         this.accerleration = 0.2;
+        this.maxSpeed = 3;
+        this.friction = 0.05;
+        this.angle = 0;
         this.controls = new Controls();
     }
     update() {
-        if (this.controls.forward) this.y -= 2;
-        if (this.controls.backward) this.y += 2;
-        if (this.controls.left) this.x -= 2;
-        if (this.controls.right) this.x += 2;
+        this._move();
+    }
+    _move() {
+        if (this.controls.forward) this.speed += this.accerleration;
+        if (this.controls.backward) this.speed -= this.accerleration;
+        if (this.speed != 0) {
+            const flip = this.speed > 0 ? 1 : -1;
+            if (this.controls.left) this.angle += 0.03 * flip;
+            if (this.controls.right) this.angle -= 0.03 * flip;
+        }
+        if (this.speed > this.maxSpeed) this.speed = this.maxSpeed;
+        if (this.speed < -this.maxSpeed / 2) this.speed = -this.maxSpeed / 2;
+        if (this.speed > 0) this.speed -= this.friction;
+        if (this.speed < 0) this.speed += this.friction;
+        if (Math.abs(this.speed) < this.friction) this.speed = 0;
+        this.x -= Math.sin(this.angle) * this.speed;
+        this.y -= Math.cos(this.angle) * this.speed;
     }
     draw(ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
         ctx.beginPath();
-        ctx.rect(this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
+        ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
         ctx.fill();
+        ctx.restore();
     }
 }
 
